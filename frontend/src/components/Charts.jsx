8e2389458ba5d00d81b3f1c620085405c00e9c53
abg287 <abg287@nau.edu>
2024-08-05 21:46:17 -0700

    n

--- frontend/src/components/Charts.jsx
+++ frontend/src/components/Charts.jsx
@@ -1,16 +1,35 @@
+
 // Imports
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Chart as ChartJS } from "chart.js/auto";
 import { Pie, Bar, Line } from "react-chartjs-2";
 import { colors } from "../colors.js"
 
 function Charts( { monthSum, monthIdx } ) {
-
+  
+  // const [ categories, setCategories ] = useState([]);
+  // const [ catAmounts, setCatAmounts ] = useState([]);
+  // const [ labels, setLabels ] = useState([]);
+  // const [ amounts, setAmounts ] = useState([]);
+  // const [ categories, setCategories ] = useState( monthSum?.categoryTotals.map( cat => cat.title ) );
   const categories = monthSum?.categoryTotals.map( cat => cat.title );
+  // const [ catAmounts, setCatAmounts ] = useState( monthSum?.categoryTotals.map( cat => cat.amount ) );
   const catAmounts = monthSum?.categoryTotals.map( cat => cat.amount );
+  // const [ labels, setLabels ] = useState( monthSum?.dailyTotals.map( item => item.day ) );
   const labels = monthSum?.dailyTotals.map( item => item.day );
+  // const [ amounts, setAmounts ] = useState( monthSum?.dailyTotals.map( item => item.total ) );
   const amounts = monthSum?.dailyTotals.map( item => item.total );
 
+  // let d = monthSum?.categoryTotals.map( cat => cat.title );
+  // console.log( d );
+
+//   useEffect( () => {
+//     setCategories( prev => [ ...prev, d ] );
+//     setCatAmounts( prev => [ ...prev, monthSum?.categoryTotals.map( cat => cat.amount ) ] );
+//     setLabels( prev => [ ...prev, monthSum?.dailyTotals.map( item => item.day ) ] );
+//     setAmounts( prev => [ ...prev, monthSum?.dailyTotals.map( item => item.total ) ] );
+//   }, [] );
+// console.log( categories );
   return (
     <div className="charts">
       <Bar
@@ -36,6 +55,7 @@ function Charts( { monthSum, monthIdx } ) {
         }}
       />
       <Line
+
         data={{
           labels: labels,
           datasets: [
